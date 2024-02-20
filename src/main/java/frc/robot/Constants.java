f7d527b2a9205aeaeca6579a6068b16563ecc6f2
Ahmed Al-Taiar <ahmed.altaiar.06@gmail.com>
2024-02-20 15:14:32 -0500

    Move subsystem file

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -11,21 +11,24 @@ import edu.wpi.first.math.util.Units;
 import swervelib.math.Matter;
 
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean constants. This
- * class should not be used for any other purpose. All constants should be declared globally (i.e. public static). Do
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean constants. This
+ * class should not be used for any other purpose. All constants should be
+ * declared globally (i.e. public static). Do
  * not put anything functional in this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants
-{
+public final class Constants {
   public static final double ROBOT_MASS = Units.lbsToKilograms(110);
-  public static final Matter CHASSIS = new Matter(new Translation3d(0, 0, Units.inchesToMeters(8)), ROBOT_MASS);
-  public static final double LOOP_TIME  = 0.13; //s, 20ms + 110ms sprk max velocity lag
+  public static final Matter CHASSIS = new Matter(
+      new Translation3d(Units.inchesToMeters(28), Units.inchesToMeters(32), Units.inchesToMeters(8)), ROBOT_MASS);
+  public static final double LOOP_TIME = 0.13; // s, 20ms + 110ms sprk max velocity lag
 
-  public static final class AutonConstants
-  {
+  public static final class AutonConstants {
     public static final PIDConstants TRANSLATION_PID = new PIDConstants(0.7, 0, 0);
     public static final PIDConstants ANGLE_PID = new PIDConstants(0.4, 0, 0.01);
   }
@@ -46,14 +49,13 @@ public final class Constants
     public static final double kP = 0.004;
     public static final double kI = 0;
     public static final double kD = 1.5;
-    
+
     public static final double kS = 0;
     public static final double kG = 0;
     public static final double kV = 0;
   }
 
-  public static class OperatorConstants
-  {
+  public static class OperatorConstants {
     // Joystick Deadband
     public static final double LEFT_X_DEADBAND = 0.1;
     public static final double LEFT_Y_DEADBAND = 0.1;
