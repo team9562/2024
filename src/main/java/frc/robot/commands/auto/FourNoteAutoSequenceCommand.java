913ad5cfda254179adf9db91d1a87844237892d6
xv786vx <firas.aj@icloud.com>
2024-04-03 16:13:04 -0400

    cleaned up pplib and made a center note auto
    Co-authored-by: Ahmed Al-Taiar <cy1der@users.noreply.github.com>

--- src/main/java/frc/robot/commands/auto/FourNoteAutoSequenceCommand.java
+++ src/main/java/frc/robot/commands/auto/FourNoteAutoSequenceCommand.java
@@ -1,7 +1,6 @@
 package frc.robot.commands.auto;
 
 import java.util.Optional;
-
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
